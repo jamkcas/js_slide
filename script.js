09abd9ddd783115fe934eb5f3834787c830eb2ad
James Casey <jamkcas@gmail.com>
2013-10-26 15:26:18 -0700

    Fixed an issue with the reset index of the slideshow resetting to the array length value, instead of the the last index value

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ var left = document.getElementsByClassName('arrows')[1];
 var pictures = document.getElementsByClassName('pictures');
 
 var picIndex = pictures.length - 1;
-var index2 = pictures.length;
 
 var slide, restart;
 
+var resetIndex = pictures.length;
+
 var slideShow = function() {
+
     pictures[picIndex].style.right = "-300px"; // moves picture to the right
-    pictures[picIndex].style.zIndex = 0;  // resets the z-index
 
     // resets each picture and puts it to the back
-    if(pictures[index2] && index2 >= 0) {
-      pictures[index2].style.right = 0;
-      pictures[index2].style.zIndex -= 10;
+    if(pictures[resetIndex]) {
+      pictures[resetIndex].style.zIndex -= 10;
+      pictures[resetIndex].style.right = 0;
     }
 
     // resets or updates the picture index
@@ -23,12 +24,10 @@ var slideShow = function() {
     } else {
       picIndex -= 1;
     }
-
-    // resets or updates the picture reset index
-    if(index2 == 0) {
-        index2 = pictures.length;
+    if(resetIndex == 0) {
+      resetIndex = pictures.length - 1;
     } else {
-      index2 -= 1;
+      resetIndex -= 1;
     }
 };
 
@@ -37,29 +36,29 @@ var init = function () {
   slide = setInterval(slideShow, 3000);
 };
 
-// toggles pictures to the right
-right.addEventListener('click', function(){
-  clearInterval(slide);
-  clearInterval(restart);
-  if(picIndex > 0) {
-    pictures[picIndex].style.right = "-300px";
-    pictures[picIndex].style.zIndex = 0;
-    picIndex -= 1;
-  }
-  restart = setInterval(slideShow, 3000);
-});
+// // toggles pictures to the right
+// right.addEventListener('click', function(){
+//   clearInterval(slide);
+//   clearInterval(restart);
+//   if(picIndex > 0) {
+//     pictures[picIndex].style.right = "-300px";
+//     pictures[picIndex].style.zIndex = 0;
+//     picIndex -= 1;
+//   }
+//   restart = setInterval(slideShow, 3000);
+// });
 
-// toggles pictures to the left
-left.addEventListener('click', function(){
-  clearInterval(slide);
-  clearInterval(restart);
-  if(picIndex < pictures.length - 1) {
-    pictures[picIndex + 1].style.right = 0;
-    pictures[picIndex].style.zIndex = 0;
-    picIndex += 1;
-  }
-  restart = setInterval(slideShow, 3000);
-});
+// // toggles pictures to the left
+// left.addEventListener('click', function(){
+//   clearInterval(slide);
+//   clearInterval(restart);
+//   if(picIndex < pictures.length - 1) {
+//     pictures[picIndex + 1].style.right = 0;
+//     pictures[picIndex].style.zIndex = 0;
+//     picIndex += 1;
+//   }
+//   restart = setInterval(slideShow, 3000);
+// });
 
 
 
